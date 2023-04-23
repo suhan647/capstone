b971a46f1d75e532926b18f046dd29aee38c4f42
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-04-23 23:35:14 +0530

    Carousel added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import Carousal from "./components/carousal/Carousal";
 import Header from "./components/header/Header";
-
+// import carousal from "./components/carousal"
 function App() {
   return (
     <>
       <Header />
+      <Carousal/>
     </>
   );
 }
