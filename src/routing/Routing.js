cb2ef99a5d4fabc421caafa281b0028d4e2b586c
PranaliAdsul <pranuadsulst1137@gmail.com>
2023-04-27 14:50:30 +0530

    adding WishList page

--- src/routing/Routing.js
+++ src/routing/Routing.js
@@ -5,6 +5,8 @@ import ElectronicProducts from '../components/products/ElectronicProducts'
 import MensProducts from '../components/products/MensProducts'
 import ProductDetails from '../components/products/ProductDetails'
 import WomensProducts from '../components/products/WomensProducts'
+import WishList from '../components/wishlist/Wishlist.js'
+
 
 
 function Routing() {
@@ -16,7 +18,8 @@ function Routing() {
         <Route path="/mensproducts" element={<MensProducts />}></Route>
         <Route path="/womensproducts" element={<WomensProducts />}></Route>
         <Route path="/electronicproducts" element={<ElectronicProducts />}></Route>
-        <Route path='/productdetails/:id' element={<ProductDetails/>} ></Route>
+        <Route path="/wishlist" element={<WishList />}></Route>
+        <Route path="/productdetails/:id" element={<ProductDetails/>} ></Route>
         
     </Routes>
     
