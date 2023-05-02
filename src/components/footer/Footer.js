34067b419258d79f50f3310b25882a6d913d2130
Koustav <koustavbasak94@gmail.com>
2023-05-03 00:30:08 +0530

    change the Footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,58 +1,34 @@
-import * as React from "react";
-import Container from "@mui/material/Container";
-// import shoplogo from "../Images/shoplogo.png";
-// import Image from "material-ui-image";
-import Link from "react";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
-import Paper from "@mui/material/Paper";
+import React from "react";
+import "../../App.css";
 
-export default function GuestFooter() {
+export default function Footer() {
   return (
-    <Paper
-      sx={{
-        marginTop: "calc(10% + 60px)",
-        position: "",
-        bottom: 0,
-        width: "100%",
-      }}
-      component="footer"
-      square
-      variant="outlined"
-    >
-      <Container maxWidth="lg">
-        <Box
-          sx={{
-            flexGrow: 1,
-            justifyContent: "center",
-            display: "flex",
-            my: 1,
-          }}
-        >
-          {/* <Link href="/">
-            <Image
-              priority
-              src="images/shoplogo.png"
-              width={75}
-              height={30}
-              alt="Logo"
-            />
-          </Link> */}
-        </Box>
-
-        <Box
-          sx={{
-            flexGrow: 1,
-            justifyContent: "center",
-            display: "flex",
-            mb: 2,
-          }}
-        >
-          <Typography variant="caption" color="initial">
-            Copyright ©2022. [] Limited
-          </Typography>
-        </Box>
-      </Container>
-    </Paper>
+    <footer className="footer">
+      <div className="footer-container">
+        <div className="footer-links">
+          <div className="footer-section">
+            <h3>City Shop</h3>
+            <a href="#">About Us</a>
+            <a href="#">Mens Products</a>
+            <a href="#">Womens Products</a>
+          </div>
+          <div className="footer-section">
+            <h4>Support</h4>
+            <a href="#">Customer care Number</a>
+            <p>+91-9038497002</p>
+            <a href="#">Email Us</a>
+            <a href="#">Refund Policy</a>
+          </div>
+          <div className="footer-section">
+            <h4>Partnerships</h4>
+            <a href="#">Become an affiliate Partner</a>
+            <a href="#">Check order Status</a>
+          </div>
+        </div>
+        <div className="footer-bottom">
+          <p>Copyright ©2023. [] Limited</p>
+        </div>
+      </div>
+    </footer>
   );
 }
